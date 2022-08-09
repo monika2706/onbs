6d17792ef7b98c68d33f2d50de72b88575fbd785
monikaramesh <monikaramesh2706@gmail.com>
2022-08-09 16:00:03 +0530

    first commit

---
+++ src/main/java/onbs/onbs/OnbsApplication.java
@@ -0,0 +1,13 @@
+package onbs.onbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onbs/onbs/OnbsApplicationTests.java
@@ -0,0 +1,13 @@
+package onbs.onbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
